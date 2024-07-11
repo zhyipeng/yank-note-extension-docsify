e8f89a347038ba6d6dfd729ec4848a63dfc2bc8d
purocean <purocean@gmail.com>
2024-07-11 22:54:53 +0800

    feat: 更换 CDN 地址
    1. 使用国内更容易访问的 CDN
    2. 带上 https 协议头，方便在新版 Yank Note 中直接打开 index.html 文件

--- src/templates.ts
+++ src/templates.ts
@@ -7,7 +7,7 @@ export const IndexHtml = (sidebar: boolean) => {
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify/themes/vue.css">
+  <link rel="stylesheet" href="https://fastly.jsdelivr.net/npm/docsify/themes/vue.css">
 </head>
 <body>
   <div id="app"></div>
@@ -16,7 +16,7 @@ export const IndexHtml = (sidebar: boolean) => {
       loadSidebar: ${sidebar},
     }
   </script>
-  <script src="//cdn.jsdelivr.net/npm/docsify/lib/docsify.min.js"></script>
+  <script src="https://fastly.jsdelivr.net/npm/docsify/lib/docsify.min.js"></script>
 </body>
 </html>`
 }
